a9d9178ff07e01845f509af7161b0c9307e0a2c2
Engeniy Filatov <wtfil@yandex-tem.ru>
2013-05-15 12:33:38 +0400

    fixed proxying of css and js files

--- blocks/i-ycssjs/i-ycssjs.priv.js
+++ blocks/i-ycssjs/i-ycssjs.priv.js
@@ -12,7 +12,7 @@
             'gif': 'image/gif',
             'jpg': 'image/jpg'
         },
-        route = new RegExp('^.*\.(' + Object.keys(CONTENT_TYPES).join('|') + ')$');
+        route = new RegExp('^.*\\.(' + Object.keys(CONTENT_TYPES).join('|') + ')$');
 
     BEM.blocks['i-router'].define('GET', route, 'i-ycssjs');
     BEM.decl({block: 'i-ycssjs'}, null, {
@@ -22,11 +22,16 @@
                 suffix = matches[1],
                 fileDir = path.replace(/[^\/]+$/, '');
 
+            if (suffix === 'css' || suffix === 'js') {
+                path = path.replace(/_([\w\.]+$)/, '$1');
+            }
+
             fs.stat(path, function (err, stat) {
 
                 if (err || !stat.isFile()) {
                     res.statusCode = 503;
                     res.end('Not file');
+                    return;
                 }
 
                 res.setHeader('Content-Type', CONTENT_TYPES[suffix]);
@@ -34,12 +39,12 @@
                 if (suffix === 'css') {
 
                     borschik({
-                        input: path.replace(/_([\w\.]+$)/, '$1'),
+                        input: path,
                         output: res,
                         tech: 'css',
                         minimize: false,
                         freeze: false
-                    }).then(null, function(e) {
+                    }).then(null, function (e) {
                         res.statusCode = 503;
                         console.log(e.stack);
                         res.end(String(e));
@@ -63,7 +68,7 @@
                                     return [
                                         '/* start: ' + path + '*/\n',
                                         fs.readFileSync(path, 'utf8'),
-                                        '/* end: ' + path + '*/\n\n',
+                                        '/* end: ' + path + '*/\n\n'
                                     ].join('');
                                 } catch (e) {
                                     console.log(e.message);
