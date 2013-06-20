4ad8a2da30b7bd93dbb75cce2519abb3f9fc952d
wtfil <filmuzhik@gmail.com>
2013-06-20 13:20:26 +0300

    unescapeHTML and escapeHTML now working on client side

--- blocks/i-router/__params/i-router__params.priv.js
+++ blocks/i-router/__params/i-router__params.priv.js
@@ -12,38 +12,6 @@
 
         MAX_POST_BODY_SIZE: 1024 * 1024,
 
-        /**
-         * Escape html special chars
-         *
-         * @param {String} html
-         * @return {String} text
-         */
-        escapeHTML: function (html) {
-            return String(html)
-                .replace(/&/g, '&amp;')
-                .replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;')
-                .replace(/"/g, '&quot;')
-                .replace(/'/g, '&#x27;')
-                .replace(/\//g, '&#x2F;');
-        },
-
-        /**
-         * Unescape html special chars
-         *
-         * @param {String} text
-         * @return {String} html
-         */
-        unescapeHTML: function (text) {
-            return String(text)
-                .replace(/&amp;/g, '&')
-                .replace(/&lt;/g, '<')
-                .replace(/&gt;/g, '>')
-                .replace(/&quot;/g, '"')
-                .replace(/&#x27;/g, '\'')
-                .replace(/&#x2F;/g, '/');
-        },
-
         _execHandler: function (handler) {
             var base = this.__base,
                 _this = this,
