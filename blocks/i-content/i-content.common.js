df5e28aeed42298ef045a58a8bdd335134356d1a
Engeniy Filatov <wtfil@yandex-tem.ru>
2013-06-26 15:52:12 +0400

    bemhmtl templates now will be avalible with {block: "i-content", mods: {type: "bemhtml"}} in bemdecl file

--- blocks/i-content/i-content.common.js
+++ blocks/i-content/i-content.common.js
@@ -2,42 +2,39 @@
  * Convert bemjson parts to content
  *
  */
-BEM.decl('i-content', null, {
+(function () {
+    var error = new Error("You should add {block: 'i-content', mods: {type: 'bemhtml'}} into .bemdecl.js file to use bemhtml templates or some other type you want");
 
-    /**
-     * Process given bemjson to html
-     *
-     * @param {String|Object|Array} bemJson
-     * @param {Boolean} [isSync] if true, method will perform synchronous BEM.JSON.build and return a html string
-     * @return {Vow.promise|String}
-     */
-    html: function (bemJson, isSync) {
-        if (!Array.isArray(bemJson)) {
-            bemJson = [bemJson];
-        }
-        return (isSync) ? this._htmlSync(bemJson) : this._htmlAsync(bemJson);
-    },
+    BEM.decl('i-content', null, {
+        
+        /**
+         * Process given bemjson to html
+         *
+         * @abstract
+         *
+         * @param {String|Object|Array} bemJson
+         * @param {Boolean} [isSync] if true, method will perform synchronous BEM.JSON.build and return a html string
+         * @return {Vow.promise|String}
+         */
+        html: function () {
+            throw error;
+        },
+        
+        /**
+         * @abstract
+         */
+        _htmlSync: function () {
+            throw error;
+        },
 
-    _htmlSync: function (json) {
-        return BEMHTML.call(BEM.JSON.build(json));
-    },
-
-    _htmlAsync: function (json) {
-        var promise;
-
-        if (typeof (json) === 'string') {
-            promise = Vow.fulfill(json);
-        } else {
-            promise = Vow.promise();
-            BEM.JSON.buildAsync(
-                json,
-                function (result) {
-                    promise.fulfill(BEMHTML.call(result));
-                }
-            );
+        /**
+         * @abstract
+         */
+        _htmlAsync: function () {
+            throw error;
         }
 
-        return promise;
-    }
+    });
 
-});
+}());
+
